77c2ef2c374bf0d5631750c5d710aca854db58ed
laskydev <eduardofortuna99@gmail.com>
2021-02-08 06:43:38 -0600

    36 Retornar elementos del componen

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { Fragment } from "react";
 
+//Un componente debe retornar 1 solo contenedor y entre parentesis
 const PrimerApp = () => {
-    return <h1>Hola Mundo</h1>;
+    return (
+        //Se puede retornar un fragment o un div o no colocar nada
+        <Fragment>
+            <h1>Hola Mundo</h1>
+            <p>Mi primera aplicación</p>
+        </Fragment>
+    );
 };
 
 export default PrimerApp;

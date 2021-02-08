fb3e575a42d50e384e226c0d240c0ae166ad9029
laskydev <eduardofortuna99@gmail.com>
2021-02-08 11:36:57 -0600

    40 Default params

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,18 +1,21 @@
 import React, { Fragment } from "react";
 import PropTypes from "prop-types";
-const PrimerApp = ({ saludo }) => {
+const PrimerApp = ({ saludo, subtitulo }) => {
     return (
         <Fragment>
             <h1>{saludo}</h1>
             {/* <pre>{JSON.stringify(saludo, null, 3)}</pre> */}
+            <p>{subtitulo}</p>
         </Fragment>
     );
 };
 
-//Para poder forzar que manden el dato necesario se utiliza PropTypes | Es necesario importarlo
 PrimerApp.propTypes = {
-    // Se coloca el nombre del elemento que se validará, si se tienen más elementos simplemente se agregan a este objeto
     saludo: PropTypes.string.isRequired,
+};
+
+PrimerApp.defaultProps = {
+    subtitulo: "Subtitulo por defecto",
 };
 
 export default PrimerApp;

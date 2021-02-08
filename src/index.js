38ddebf263295b575d4eda5473240d9082c8db8f
laskydev <eduardofortuna99@gmail.com>
2021-02-08 12:55:18 -0600

    42 Evento click

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import PrimerApp from "./PrimeraApp";
+import CounterApp from "./components/CounterApp";
 import "./index.css";
 
 const divRoot = document.querySelector("#root");
 
 // Para enviar elementos del index a los componentes se hace de la siguiente manera:
-ReactDOM.render(<PrimerApp saludo="Hola mundo" />, divRoot);
+ReactDOM.render(<CounterApp value={1} />, divRoot);
